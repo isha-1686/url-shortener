d0e30dfe48800efac3d04f8eb6a2e06f73122cae
Isha katiyar <ishakatiyar1608@gmail.com>
2026-06-14 15:17:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     setLoading(true);
     setError("");
     try {
-      const response = await fetch("https://url-shortener-v5xu.onrender.com", {
+      const response = await fetch("https://url-shortener-v5xu.onrender.com/shorten", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url }),
